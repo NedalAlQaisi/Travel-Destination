5ea1f48ce4096240f045a625db34434bd00f449e
NedalAlQaisi <n.alqaisi221999@gmail.com>
2022-02-14 02:30:08 +0200

    Set Up src

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,19 @@
+import './Footer.css'
+
+function Footer() {
+    return (
+    <footer>
+		 
+        <p> Nedal AL-Qaisi © copyright </p>
+        <div>
+            <a href='/'>facebook</a>
+            <a href='/'>twitter</a>
+            <a href='/'>google</a>
+            <a href='/'>linkedin</a>
+        </div>
+	</footer>
+
+    
+    )
+}
+export default Footer;
