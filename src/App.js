ccbc6c1947d5eb20eff8e94bfd39d8d37d9b52bc
NedalAlQaisi <n.alqaisi221999@gmail.com>
2022-02-14 19:58:36 +0200

    Fix your import paths in the Home component, and import dp.json in the App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import Home from "./components/home/Home";
+const citiesData = require('./data/db.json');
 
 
 function App() {
     return (
     <>
-        <Home />
+        <Home citiesData = {citiesData}/>
     </> 
     );
 }
